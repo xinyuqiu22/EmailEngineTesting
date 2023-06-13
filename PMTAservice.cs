3062154a9e2395799c3891ff54ed7ad8b3332e37
Xinyu Qiu <qiux1@seattleu.edu>
2023-06-13 14:56:41 -0700

    Send up the ground level of Insight.Database in the new email engine. Changed in PMTAservice.cs and Program.cs

---
+++ PMTAservice.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Data;
+using Insight.Database;
+using System.Web.Http;
+using EASendMail;
+
+namespace EmailEngineTesting
+{
+
+    public class PMTAservice
+    {
+        public class EmailProcessorModel
+        {
+
+            public SmtpMail oMail { get; set; }
+            public SmtpServer oServer { get; set; }
+            public string PURLresponse { get; set; }
+            public string[] responseArray { get; set; }
+            public string OutboundDomainName { get; set; }
+            public int EmailBatch_ID { get; set; }
+            public string SubjectLine { get; set; }
+            public string PURL { get; set; }
+            public string ResponseCode { get; set; }
+            public string PromoCode { get; set; }
+            public string EmailAddress { get; set; }
+        }
+
+        public class EngineSettings
+        {
+            public int EmailServiceProviderGroup_ID { get; set; }
+            public string OutboundDomainName { get; set; }
+            public string OutboundUsername { get; set; }
+            public string OutboundPP { get; set; }
+            public string OutboundServerAddress { get; set; }
+            public int MinutesLeftInWeek { get; set; }
+            public int HourlyEmailLimit { get; set; }
+            public int FailedEmailCount { get; set; }
+        }
+
+        public class RecipientModel
+        {
+            public int EmailBatch_ID { get; set; }
+            public string FromEmailAddress { get; set; }
+            public string ReplyToEmailAddress { get; set; }
+            public string SubjectLine { get; set; }
+            public string TemplateURL { get; set; }
+            public string Unsubscribe { get; set; }
+            public bool ClickTracking { get; set; }
+            public string LastName { get; set; }
+            public string FirstName { get; set; }
+            public string MiddleInitial { get; set; }
+            public string ResponseCode { get; set; }
+            public string PromoCode { get; set; }
+            public string EmailAddress { get; set; }
+            public string result { get; set; }
+            public string filter { get; set; }
+            public decimal OfferPayment { get; set; }
+        }
+
+        public static void sendPMTA(int EmailServiceProvider_ID, DateTime DropDate, bool Realtime)
+        {
+            List<EngineSettings> ES;
+            int SendCycle = 0;
+            int DropIndex = 0;
+            decimal WeekCount = 0;
+            int CurrentEmailBatchID = 0;
+            decimal SendIntervalSeconds = 600;
+            DateTime AppStartTimestamp = DateTime.Now;
+            string connectionString = ConfigurationManager.ConnectionStrings["DataCenterEmailEngine"].ConnectionString;
+
+            using (IDbConnection EmailDrop = new SqlConnection(connectionString))
+            {
+                IEnumerable<EngineSettings> temp = EmailDrop.QuerySql<EngineSettings>("EXEC TEST_EmailBatchRecipients_Get @EmailServiceProvider_ID", new { EmailServiceProvider_ID }).ToList();
+                CurrentEmailBatchID = EmailDrop.QuerySql<int>(
+                    "EXEC WeeklyEmailBatches_GetNext @DropDate, @Realtime, @EmailServiceProvider_ID, @Processor_ID",
+                    new { DropDate, Realtime, EmailServiceProvider_ID, Processor_ID = 1 }).Single();
+
+                WeekCount = Convert.ToDecimal(EmailDrop.QuerySql<int>(
+                    "EXEC WeeklyEmailBatchRecipients_GetCountV2 @EmailServiceProvider_ID, @Realtime",
+                    new { EmailServiceProvider_ID, Realtime }).Single());
+
+
+                List<RecipientModel> TheDrop = EmailDrop.QuerySql<RecipientModel>(
+                    "EXEC WeeklyEmailBatchRecipients_GetV3 @EmailServiceProvider_ID, @EmailBatch_ID, @Realtime",
+                    new { EmailServiceProvider_ID, EmailBatch_ID = CurrentEmailBatchID, Realtime }).ToList();
+
+            }
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,91 +1,22 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using EASendMail;
 using System;
-using System.Configuration;
 using System.Collections.Generic;
-using System.Data;
-using Insight.Database;
-using System.Web.Http;
-using EASendMail;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 
-namespace TestingORM
+
+namespace EmailEngineTesting
 {
-    public class EmailProcessorModel
-    {
-
-        public SmtpMail oMail { get; set; }
-        public SmtpServer oServer { get; set; }
-        public string PURLresponse { get; set; }
-        public string[] responseArray { get; set; }
-        public string OutboundDomainName { get; set; }
-        public int EmailBatch_ID { get; set; }
-        public string SubjectLine { get; set; }
-        public string PURL { get; set; }
-        public string ResponseCode { get; set; }
-        public string PromoCode { get; set; }
-        public string EmailAddress { get; set; }
-    }
-
-    public class EngineSettings
-    {
-        public int EmailServiceProviderGroup_ID { get; set; }
-        public string OutboundDomainName { get; set; }
-        public string OutboundUsername { get; set; }
-        public string OutboundPP { get; set; }
-        public string OutboundServerAddress { get; set; }
-        public int MinutesLeftInWeek { get; set; }
-        public int HourlyEmailLimit { get; set; }
-        public int FailedEmailCount { get; set; }
-    }
-
-    public class RecipientModel
-    {
-        public int EmailBatch_ID { get; set; }
-        public string FromEmailAddress { get; set; }
-        public string ReplyToEmailAddress { get; set; }
-        public string SubjectLine { get; set; }
-        public string TemplateURL { get; set; }
-        public string Unsubscribe { get; set; }
-        public bool ClickTracking { get; set; }
-        public string LastName { get; set; }
-        public string FirstName { get; set; }
-        public string MiddleInitial { get; set; }
-        public string ResponseCode { get; set; }
-        public string PromoCode { get; set; }
-        public string EmailAddress { get; set; }
-        public string result { get; set; }
-        public string filter { get; set; }
-        public decimal OfferPayment { get; set; }
-    }
+    
     class Program
     {
         static void Main(string[] args)
         {
             int EmailServiceProvider_ID = 0;
-            InsightDatabaseTest(EmailServiceProvider_ID);
+            PMTAservice.sendPMTA(EmailServiceProvider_ID, DateTime.Now, false);
         }
 
-        public static void sendPMTA(int EmailServiceProvider_ID, DateTime DropDate, bool Realtime)
-        {
-
-        }
-
-        public static void InsightDatabaseTest(int EmailServiceProvider_ID)
-        {
-            // Retrieve the connection string from the configuration file
-            string connectionString = ConfigurationManager.ConnectionStrings["DataCenterEmailEngine"].ConnectionString;
-
-            // Create a new Microsoft.Data.SqlClient.SqlConnection using the connection string
-            using (IDbConnection connection = new Microsoft.Data.SqlClient.SqlConnection(connectionString))
-            {
-                // Execute the stored procedure TEST_EmailBatchRecipients_Get using Insight.Database's Query method
-                // Pass the EmailServiceProvider_ID as a parameter and specify the command type as StoredProcedure
-                // Map the results to a List of EngineSettings objects
-                IEnumerable<EngineSettings> ES = connection.QuerySql<EngineSettings>("EXEC TEST_EmailBatchRecipients_Get @EmailServiceProvider_ID", new { EmailServiceProvider_ID }).ToList();
-                Console.WriteLine(ES.Count());
-            }
-        }
-
-       
-
+        
     }
 }
