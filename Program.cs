02af3ef7c675519ab99677019b7c183153a715ea
Xinyu Qiu <qiux1@seattleu.edu>
2023-06-21 11:00:03 -0700

    CHange build email body to async

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace EmailEngineTesting
         static void Main(string[] args)
         {
             int EmailServiceProvider_ID = 0;
-            DateTime Testtime = new DateTime(2023, 3, 23);
+            DateTime Testtime = DateTime.Now;
             //PMTAservice.sendPMTA(EmailServiceProvider_ID, Testtime, false).GetAwaiter().GetResult();
             MailGunService.SendMailgunAsync(EmailServiceProvider_ID, Testtime).GetAwaiter().GetResult();
         }
